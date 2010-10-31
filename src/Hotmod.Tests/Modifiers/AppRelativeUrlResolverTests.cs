678bc8d6fb2894f9aeb4c3cf2e19573bfb7a7f95
Andrew Davey <andrew@equin.co.uk>
2010-10-31 14:48:41 +0000

    Add AppRelativeUrlResolver.

---
+++ src/Hotmod.Tests/Modifiers/AppRelativeUrlResolverTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Web;
+using System.Xml.Linq;
+using Moq;
+using Xunit;
+
+namespace Hotmod.Modifiers
+{
+    public class AppRelativeUrlResolverTests
+    {
+        [Fact]
+        public void a_href_url_resolved()
+        {
+            var document = XDocument.Parse("<html><a href=\"~/hello\">test</a></html>");
+            var modifier = new AppRelativeUrlResolver();
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpRequest.Setup(r => r.ApplicationPath).Returns("/root");
+            modifier.HttpRequest = httpRequest.Object;
+
+            var output = modifier.Modify(document);
+
+            var href = output.Root.Element("a").Attribute("href").Value;
+            Assert.Equal("/root/hello", href);
+        }
+
+        [Fact]
+        public void link_href_url_resolved()
+        {
+            var document = XDocument.Parse("<html><link href=\"~/hello\"/></html>");
+            var modifier = new AppRelativeUrlResolver();
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpRequest.Setup(r => r.ApplicationPath).Returns("/root");
+            modifier.HttpRequest = httpRequest.Object;
+
+            var output = modifier.Modify(document);
+
+            var href = output.Root.Element("link").Attribute("href").Value;
+            Assert.Equal("/root/hello", href);
+        }
+
+        [Fact]
+        public void img_src_url_resolved()
+        {
+            var document = XDocument.Parse("<html><img src=\"~/hello\"/></html>");
+            var modifier = new AppRelativeUrlResolver();
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpRequest.Setup(r => r.ApplicationPath).Returns("/root");
+            modifier.HttpRequest = httpRequest.Object;
+
+            var output = modifier.Modify(document);
+
+            var href = output.Root.Element("img").Attribute("src").Value;
+            Assert.Equal("/root/hello", href);
+        }
+
+
+        [Fact]
+        public void script_src_url_resolved()
+        {
+            var document = XDocument.Parse("<html><script src=\"~/hello\"></script></html>");
+            var modifier = new AppRelativeUrlResolver();
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpRequest.Setup(r => r.ApplicationPath).Returns("/root");
+            modifier.HttpRequest = httpRequest.Object;
+
+            var output = modifier.Modify(document);
+
+            var href = output.Root.Element("script").Attribute("src").Value;
+            Assert.Equal("/root/hello", href);
+        }
+    }
+}
---
+++ src/Hotmod/Modifiers/AppRelativeUrlResolver.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Hotmod.Modifiers
+{
+    public class AppRelativeUrlResolver : IModifier
+    {
+        public XDocument Modify(XDocument document)
+        {
+            var elementsWithHref = document.Descendants()
+                .Where(e => e.Name.LocalName == "a" || e.Name.LocalName == "link");
+            var elementsWithSrc = document.Descendants()
+                .Where(e => e.Name.LocalName == "img" || e.Name.LocalName == "script");
+            var root = HttpRequest.ApplicationPath.TrimEnd('/');
+            
+            foreach (var element in elementsWithHref)
+            {
+                ExpandUrl(element, "href", root);
+            }
+            foreach (var element in elementsWithSrc)
+            {
+                ExpandUrl(element, "src", root);
+            }
+
+            return document;
+        }
+
+        public HttpRequestBase HttpRequest
+        {
+            get
+            {
+                if (httpRequest == null) httpRequest = new HttpRequestWrapper(HttpContext.Current.Request);
+                return httpRequest;
+            }
+            set { httpRequest = value; }
+        }
+
+        HttpRequestBase httpRequest;
+
+        void ExpandUrl(XElement element, string attributeName, string root)
+        {
+            var href = element.Attribute(attributeName);
+            if (href != null && href.Value.StartsWith("~"))
+            {
+                href.Value = root + href.Value.Substring(1);
+            }
+        }
+    }
+}
